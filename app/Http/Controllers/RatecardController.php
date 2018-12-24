6acab35f65723e1fe9540c0c2222fd37beb3a4d6
billhoang <programming.iter@gmail.com>
2018-12-25 00:03:30 +0700

    Apply filter, paginate, ajax loading for Ratecard Page

--- app/Http/Controllers/RatecardController.php
+++ app/Http/Controllers/RatecardController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\meters;
+use App\ListUltilAjaxTable;
 
 class RatecardController extends Controller
 {
@@ -16,25 +17,119 @@ class RatecardController extends Controller
      */
     public function index()
     {
-        $meters = new meters;
-        if(request()->has('MeterRegion') && request('MeterRegion') != 'All'){
-            $meters = $meters->where('MeterRegion', request('MeterRegion'));
-        }
-        
-        if(request()->has('MeterCategory') && request('MeterCategory') != 'All'){
-            $meters = $meters->where('MeterCategory', request('MeterCategory'));
-        }
-        
-        $meters = $meters->paginate(40)->appends([
-            'MeterRegion' => request('MeterRegion'),
-            'MeterCategory' => request('MeterCategory')
-        ]);
-
         //filter data
         $regions = DB::table('meters')->select('MeterRegion')->where('MeterRegion', '<>', '')->groupBy('MeterRegion')->get();
         $categories = DB::table('meters')->select('MeterCategory')->groupBy('MeterCategory')->get();
+        $subcategories = DB::table('meters')->select('MeterSubCategory')->where('MeterSubCategory', '<>', '')->groupBy('MeterSubCategory')->get();
+        $names = DB::table('meters')->select('MeterName')->groupBy('MeterName')->get();
 
-		return view("ratecard", compact(['meters', 'regions', 'categories']));
+		return view("ratecard", compact(['meters', 'regions', 'categories', 'subcategories', 'names']));
+    }
+
+    public function getResourceRateCard(){
+        $meters = DB::table('meters')->get();
+
+        $data = $alldata = json_decode(json_encode($meters));
+
+        $datatable = array_merge(['pagination' => [], 'sort' => [], 'query' => []], $_REQUEST);
+
+        // search filter by keywords
+        $filter = isset($datatable['query']['generalSearch']) && is_string($datatable['query']['generalSearch'])
+            ? $datatable['query']['generalSearch'] : '';
+        if ( ! empty($filter)) {
+            $data = array_filter($data, function ($a) use ($filter) {
+                return (boolean)preg_grep("/$filter/i", (array)$a);
+            });
+            unset($datatable['query']['generalSearch']);
+        }
+
+        // filter by field query
+        $query = isset($datatable['query']) && is_array($datatable['query']) ? $datatable['query'] : null;
+        if (is_array($query)) {
+            $query = array_filter($query);
+            foreach ($query as $key => $val) {
+                $data = $this->list_filter($data, [$key => $val]);
+            }
+        }
+
+        $sort  = ! empty($datatable['sort']['sort']) ? $datatable['sort']['sort'] : 'asc';
+        $field = ! empty($datatable['sort']['field']) ? $datatable['sort']['field'] : 'RecordID';
+
+        $meta    = [];
+        $page    = ! empty($datatable['pagination']['page']) ? (int)$datatable['pagination']['page'] : 1;
+        $perpage = ! empty($datatable['pagination']['perpage']) ? (int)$datatable['pagination']['perpage'] : -1;
+
+        $pages = 1;
+        $total = count($data); // total items in array
+
+        // sort
+        usort($data, function ($a, $b) use ($sort, $field) {
+            if ( ! isset($a->$field) || ! isset($b->$field)) {
+                return false;
+            }
+
+            if ($sort === 'asc') {
+                return $a->$field > $b->$field ? true : false;
+            }
+
+            return $a->$field < $b->$field ? true : false;
+        });
+
+        // $perpage 0; get all data
+        if ($perpage > 0) {
+            $pages  = ceil($total / $perpage); // calculate total pages
+            $page   = max($page, 1); // get 1 page when $_REQUEST['page'] <= 0
+            $page   = min($page, $pages); // get last page when $_REQUEST['page'] > $totalPages
+            $offset = ($page - 1) * $perpage;
+            if ($offset < 0) {
+                $offset = 0;
+            }
+
+            $data = array_slice($data, $offset, $perpage, true);
+        }
+
+        $meta = [
+            'page'    => $page,
+            'pages'   => $pages,
+            'perpage' => $perpage,
+            'total'   => $total,
+        ];
+
+
+        // if selected all records enabled, provide all the ids
+        if (isset($datatable['requestIds']) && filter_var($datatable['requestIds'], FILTER_VALIDATE_BOOLEAN)) {
+            $meta['rowIds'] = array_map(function ($row) {
+                return $row->RecordID;
+            }, $alldata);
+        }
+
+
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+
+        $result = [
+            'meta' => $meta + [
+                    'sort'  => $sort,
+                    'field' => $field,
+                ],
+            'data' => $data,
+        ];
+
+        echo json_encode($result, JSON_PRETTY_PRINT);
+        exit();
+    }
+
+    public function list_filter( $list, $args = array(), $operator = 'AND' )
+    {
+        if ( ! is_array( $list ) ) {
+            return array();
+        }
+
+        $util = new ListUltilAjaxTable( $list );
+
+        return $util->filter( $args, $operator );
     }
 
     /**
